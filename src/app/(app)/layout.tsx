940de8164ef4e1d6d36cb7e13e087e1c7c3027fb
jotape <jaumjaopdro@gmail.com>
2026-03-26 16:06:41 -0300

    Show comic background behind app shell

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,9 +8,9 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
   await requireCurrentResident();
 
   return (
-    <div className="flex min-h-screen flex-col md:flex-row relative overflow-x-hidden w-full bg-neo-bg">
+    <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden md:flex-row">
       <Navbar />
-      <main className="relative z-10 mx-auto w-full max-w-[100vw] flex-1 px-3 pb-28 pt-6 sm:px-4 sm:pt-8 md:mx-0 md:max-w-none md:px-6 md:pb-24 md:pt-8 lg:px-8 lg:pt-10 xl:px-12 xl:pt-12 2xl:mx-auto 2xl:max-w-[1500px] 2xl:px-14">
+      <main className="relative z-10 mx-auto w-full max-w-[100vw] flex-1 bg-transparent px-3 pb-28 pt-6 sm:px-4 sm:pt-8 md:mx-0 md:max-w-none md:px-6 md:pb-24 md:pt-8 lg:px-8 lg:pt-10 xl:px-12 xl:pt-12 2xl:mx-auto 2xl:max-w-[1500px] 2xl:px-14">
         {children}
         <FabWizard />
       </main>
